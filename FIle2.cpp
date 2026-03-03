33463f56a30eb1120977472bd70fd9559ee14e0f
4D1FK4 <adifkapersonal0812@gmail.com>
2026-03-03 20:17:20 +0700

    Input

--- FIle2.cpp
+++ FIle2.cpp
@@ -1,3 +1,11 @@
 // library
 #include <iostream>
 using namespace std;
+
+// implementasi Prosedur dan Fungis
+void input() {
+    cout << "Masukkan panjang = ";
+    cin >> p;
+    cout << "Masukkan lebar = ";
+    cin >> l;
+}
